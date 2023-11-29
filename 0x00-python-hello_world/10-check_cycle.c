8cd277bfdc9e26e5446660ecb984c54d08c02333
chifundo biziweck <chifundobiziweck@gmail.com>
2023-11-28 18:09:22 -0800

    task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if linked list has a cycle
+ * @list:pointer to the head of the list
+ * Return: 1 if lonked list has a cycle or -1
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *fast = list;
+	listint_t *slow = list;
+
+	if (!list)
+		return (-1);
+
+	while (fast)
+	{
+		if (fast == slow)
+			return (1);
+		fast = fast->next;
+		if (!fast)
+		{
+			return (-1);
+		}
+		else
+		{
+			fast = fast->next;
+		}
+
+		slow = slow->next;
+	}
+
+	return (-1);
+}
