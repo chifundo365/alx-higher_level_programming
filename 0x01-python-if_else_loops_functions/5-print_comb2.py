365145407b87466f4dec5d361f9e0ac5dfdc4149
chifundo biziweck <chifundobiziweck@gmail.com>
2023-12-01 08:30:32 -0800

    task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i < 10:
-        print('{}'.format(0), end='')
-    print('{}'.format(i), end=', ')
+    print('{0:02d}, '.format(i), end='')
 else:
-    print('{}'.format(i))
+    print('{0:02d}'.format(i))
