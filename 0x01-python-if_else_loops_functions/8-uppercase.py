7951dffc116e828f43f22954d409bba715cb3d41
chifundo biziweck <chifundobiziweck@gmail.com>
2023-12-02 02:05:26 -0800

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 def uppercase(str):
     newline = ''
-    for i in range(len(str)):
-        if i == len(str) - 1:
+    ascii_value = 0
+    for i in range(len(str) + 1):
+        if str:
+            s = str[i]
+        else:
+            s = ""
+        if i == len(str) - 1 or i == len(str):
             newline = '\n'
-        ascii_value = ord(str[i])
+        if str:
+            ascii_value = ord(s)
         if ascii_value >= 97 and ascii_value <= 122:
             upper_case = chr(ascii_value - 32)
             print("{}{}".format(upper_case, newline), end="")
         else:
-            print("{}{}".format(str[i], newline), end="")
+            print("{}{}".format(s, newline), end="")
+        if newline == '\n':
+            break
+(uppercase(""))
