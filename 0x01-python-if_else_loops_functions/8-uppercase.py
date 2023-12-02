46d94d48755ab084caa6807e63b4bec16f272cbc
chifundo biziweck <chifundobiziweck@gmail.com>
2023-12-02 01:14:23 -0800

    code for task 8;

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def uppercase(str):
+    newline = ''
+    for i in range(len(str)):
+        if i == len(str) - 1:
+            newline = '\n'
+        ascii_value = ord(str[i])
+        if ascii_value >= 97 and ascii_value <= 122:
+            upper_case = chr(ascii_value - 32)
+            print("{}{}".format(upper_case, newline), end="")
+        else:
+            print("{}{}".format(str[i], newline), end="")
+
+
+uppercase("Best School 98 Battery street")
