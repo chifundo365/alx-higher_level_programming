7c1333fd676e05e302b3c2d88edabeec44560dbe
chifundo biziweck <chifundobiziweck@gmail.com>
2023-12-04 15:59:56 -0800

    final push

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import sys
     arg_len = len(sys.argv)
-    print("{}: {}".format(arg_len - 1, "arguments" if arg_len == 0 or arg_len > 1 else "argument"))
+    print("{} {}".format(arg_len - 1, "arguments" if arg_len == 0 or arg_len > 1 else "argument"))
     for i in range(1, len(sys.argv)):
-        print("{} {}".format(i, sys.argv[i]))
+        print("{}: {}".format(i, sys.argv[i]))
