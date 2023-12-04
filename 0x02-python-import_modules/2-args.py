de778d43d8dd77d50fc61d8e4f20486a231f0073
chifundo biziweck <chifundobiziweck@gmail.com>
2023-12-04 15:56:48 -0800

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) == 1:
-        print("{} arguments.".format(0))
-    else:
-        for i in range(1, len(sys.argv)):
-            print("{} {}".format(i, sys.argv[i]))
+    arg_len = len(sys.argv)
+    print("{}: {}".format(arg_len - 1, "arguments" if arg_len == 0 or arg_len > 1 else "argument"))
+    for i in range(1, len(sys.argv)):
+        print("{} {}".format(i, sys.argv[i]))
