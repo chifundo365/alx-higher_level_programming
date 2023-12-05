bc357a06e4851b856513b7050ffe2968fd79b83c
chifundo biziweck <chifundobiziweck@gmail.com>
2023-12-05 01:52:11 -0800

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     defined_names = dir(hidden_4)
     for i in range(len(defined_names)):
         if defined_names[i][0] != "_":
-                print("{}".format(define_names[i]))
+            print("{}".format(defined_names[i]))
