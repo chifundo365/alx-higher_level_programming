e9f296891207f03257a27eb33a8bee9e4d138a90
chifundo biziweck <chifundobiziweck@gmail.com>
2023-12-07 12:16:21 -0800

    task redone

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     if isinstance(matrix, list):
         for e in matrix:
-            for x in e:
-                print("{:d}".format(x), end=" " if e.index(x) != 2 else "")
-            print()
+            if (isinstance(e, list)):
+                for x in e:
+                    print("{:d}".format(x), end=" " if e.index(x) != 2 else "")
+                print()
