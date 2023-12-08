b47179405c4ee6b3a5f00517c8d973abebde632d
chifundo biziweck <chifundobiziweck@gmail.com>
2023-12-08 05:36:03 -0800

    task 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    int_list = []
+    for d in my_list:
+        if d % 2 == 0:
+            int_list.append(True)
+        else:
+            int_lsit.append(False)
+    return int_list
