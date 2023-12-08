b1d08b71e066e5f4681ab77bba3894cb68eb0573
chifundo biziweck <chifundobiziweck@gmail.com>
2023-12-08 05:12:18 -0800

    task 9 revised

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list) == 0:
-        return None
-    max_int = 0
-    for d in my_list:
-        if d > max_int:
-            max_int = d
-    return max_int
+    if isinstance(my_list, list):
+        if len(my_list) == 0:
+            return None
+        max_int = 0
+        for d in my_list:
+            if isinstance(d, int):
+                if d > max_int:
+                    max_int = d
+        return max_int
