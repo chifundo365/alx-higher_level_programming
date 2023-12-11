96a395887dbd616310e1e2e8f687b9a527b1a969
chifundo biziweck <chifundobiziweck@gmail.com>
2023-12-11 03:07:31 -0800

    task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    if len(a_dictionary) and isinstance(a_dictionary, dict):
+    if isinstance(a_dictionary, dict):
         new_dict = {k: v * 2 for k, v in a_dictionary.items()}
         return new_dict
