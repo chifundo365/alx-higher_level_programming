f21a7793ee72f151776bc7b2b6b6ad4334376ef2
chifundo biziweck <chifundobiziweck@gmail.com>
2023-12-11 04:15:43 -0800

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,4 +3,5 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     biggest = max(v for v in a_dictionary.values())
-    return (key[0] for key in a_dictionary.keys() if a_dictionary[key] == biggest)
+    key = [k for k in a_dictionary.keys() if a_dictionary[k] == biggest]
+    return key[0]
