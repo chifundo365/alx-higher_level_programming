e1ef01ee0824e65cbffa95f1fb064a821854cfce
chifundo biziweck <chifundobiziweck@gmail.com>
2023-12-19 06:15:00 -0800

    task 0 rev

--- 0x05-python-exceptions/0-main.py
+++ 0x05-python-exceptions/0-main.py
@@ -8,5 +8,5 @@ print("nb_print: {:d}".format(nb_print))
 nb_print = safe_print_list(my_list, len(my_list))
 print("nb_print: {:d}".format(nb_print))
 x = len(my_list) + 1
-nb_print = safe_print_list(my_list, 0)
+nb_print = safe_print_list(my_list, x)
 print("nb_print: {:d}".format(nb_print))
