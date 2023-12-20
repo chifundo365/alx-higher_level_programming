18bdad0b250eb181fd62a11b9f233d89e06bb866
chifundo biziweck <chifundobiziweck@gmail.com>
2023-12-20 04:06:56 -0800

    task 100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,7 +4,17 @@ import sys
 
 def safe_print_integer_err(value):
     try:
-        print("{:d}".format(value), file=sys.stderr)
+        print("{:d}".format(value))
         return True
     except (TypeError, ValueError):
+        msg = "Exception: Unknown format code 'd' for object of type"
+        if value is None:
+            print("{} 'None'".format(msg))
+            return False
+        data_types = [int, float, list, str, bool, tuple, dict]
+        for data_type in data_types:
+            if isinstance(value, data_type):
+                p = data_type[7:12]
+                print("{} {}".format(msg, p))
+                break
         return False
