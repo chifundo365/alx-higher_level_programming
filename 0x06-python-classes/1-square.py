5d1733ac92d659d963828e46980c185424803cc9
chifundo biziweck <chifundobiziweck@gmail.com>
2023-12-26 09:55:37 -0800

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This implements a square class with a constructor."""
+
+
+class Square:
+    """A square with a private attribute"""
+
+    def __init__(self, size=None):
+        """A constructor method."""
+        self.__size = size
