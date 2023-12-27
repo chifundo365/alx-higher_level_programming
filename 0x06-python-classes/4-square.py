5ea6ce57155cce32369455397af482d782b64fa7
chifundo biziweck <chifundobiziweck@gmail.com>
2023-12-27 01:26:01 -0800

    task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -12,9 +12,11 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
+    @property
     def size(self):
         return self.__size
 
+    @size.setter
     def size(self, value):
         if type(value) != int:
             raise TypeError("size must be an integer")
