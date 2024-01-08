cca7c7d8bb7b063bf9bada87d5b04172d397f644
chifundo biziweck <chifundobiziweck@gmail.com>
2024-01-08 04:57:18 -0800

    1 coreecrion

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,6 +6,14 @@ class Rectangle:
     """ class with width, height getter and setter """
 
     def __init__(self, width=0, height=0):
+        if type(width) is not int:
+            raise TypeError('width must be an integer')
+        elif width < 0:
+            raise ValueError('width must be >= 0')
+        if type(height) is not int:
+            raise TypeError('height must be an integer')
+        if height < 0:
+            raise ValueError('height must be >= 0')
         self.__width = width
         self.__height = width
 
@@ -30,5 +38,5 @@ class Rectangle:
         if type(value) is not int:
             raise TypeError('height must be an integer')
         if value < 0:
-            raise TypeError('height must be >= 0')
+            raise ValueError('height must be >= 0')
         self.__height = value
