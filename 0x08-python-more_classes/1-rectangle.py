469ba59a3bceac68b8eb6ff3584d2ac36f625cbb
chifundo biziweck <chifundobiziweck@gmail.com>
2024-01-08 04:33:26 -0800

    task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Implements a Rectangle class. """
+
+
+class Rectangle:
+    """ class with width, height getter and setter """
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = width
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if value is not int:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise TypeError('height must be >= 0')
+        self.__height = value
