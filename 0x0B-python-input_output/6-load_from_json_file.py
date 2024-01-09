8892901997e8b0e1f31f468fd2f81552c0279175
chifundo biziweck <chifundobiziweck@gmail.com>
2024-01-09 04:41:52 -0800

    task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Contains Definition of load_from_json_file. """
+
+
+def load_from_json_file(file_name):
+    """" loads a file from json and coverts it to python object
+    Args:
+        file_name: file name of the json object
+    Return: real python Object file
+    """
+    import json
+    with open(filename, encoding="utf-8") as f:
+        return json.loads(f.read())
