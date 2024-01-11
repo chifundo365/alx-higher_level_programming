d34ff7623652234daff28d4ccb94fd8ac319518b
chifundo biziweck <71874868+chifundo365@users.noreply.github.com>
2024-01-11 13:08:50 +0000

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+10-student.py
+"""
+
+
+class Student:
+    """
+    student classs with first_name, last_name and age
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        makes a dictionary of all attributes in a class
+
+        Args:
+            self: the instance itsself
+            attrs: attributes
+
+        Return: a json serialisable python object with values equal:
+                to the attributes in attrs or all if attr is None
+         """
+        obj = self
+        if hasattr(obj, '__dict__'):
+            new_dict = obj.__dict__.items()
+            if attrs:
+                return {k: v for k, v in new_dict if k in attrs}
+            return {obj.__dict__}
