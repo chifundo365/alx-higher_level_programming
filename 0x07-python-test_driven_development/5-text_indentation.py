4c451f113ec08c8be6a3d557eb3bcd25d79db712
chifundo biziweck <chifundobiziweck@gmail.com>
2024-01-18 01:39:48 -0800

    text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+5-text_indentation
+"""
+
+
+def text_indentation(text):
+    """
+    prints text and print 2 newlines after either ',', ':' or '?'
+
+    Args:
+        text: The input text to print
+
+    Raises:
+        TypeError: if the input is not a string
+
+    Returns: None
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for c in text:
+        print(c, end="")
+        if c in {":", ".", "?"}:
+            print("\n")
