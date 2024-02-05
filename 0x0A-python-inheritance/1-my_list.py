5744100588d4cfd292872d00b90ba68e226330bf
chifundo biziweck <chifundobiziweck@gmail.com>
2024-02-05 02:27:42 -0800

    my list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,5 +5,17 @@ Implements MyList class
 
 
 class MyList(list):
+    """
+    Inherits from list class
+    """
+
     def print_sorted(self):
+        """
+        prints a a list in ascending order
+
+        Args:
+            self: the instance itself
+
+        Returns: None
+        """
         print(sorted(self))
