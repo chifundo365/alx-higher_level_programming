de280b93719b9751765f82f30223896ad74ee3ef
chifundo biziweck <chifundobiziweck@gmail.com>
2024-02-04 23:52:45 -0800

    Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+implements lookup function
+"""
+
+
+def lookup(obj):
+    """
+    finds the list of available attributes and methods of an object
+    
+    Args:
+        obj: The object to find the list of attributes and methods
+
+    Returns: A list object
+    """
+    return dir(obj)
