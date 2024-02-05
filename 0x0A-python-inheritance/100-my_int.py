a6435a89a5dcef338aa857eca4edf4050830aa11
chifundo biziweck <chifundobiziweck@gmail.com>
2024-02-05 13:20:04 -0800

    My integer

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Contains a class that inherits from int
+"""
+
+
+class MyInt(int):
+    """
+    Inherits from int class and inverts == and != operators
+    """
+
+
+    def __eq__(self, other):
+        return self != other
+    
+    def __ne__(self, other):
+        return self == other
