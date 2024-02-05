3a4d5b03f967d1758f40a5df9efb127b1f67c743
chifundo biziweck <chifundobiziweck@gmail.com>
2024-02-05 01:03:22 -0800

    Exact same object

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Implements is _same_class function
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    checks if object is an instance of the specfied class
+    Args:
+        obj: the object to check if is an instance of a_class
+        a_class: class to be compared to the obj
+    Returns:
+        true - if the object is an instance of a_class
+        false- f the object is not an instance of a_class
+   """
+    return isinstance(obj, a_class)
