637391845df3cd9430f00cf6fd6af5c5da47b684
= <=>
2024-03-01 13:17:53 -0800

    Sqaure Update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -37,51 +37,51 @@ class Square(Rectangle):
         self.width = size
         self.height = size
 
-        def __str__(self):
-            return "[Square] ({}) {}/{} - {}".format(
-                self.id,
-                self.x,
-                self.y,
-                self.width
-            )
+    def __str__(self):
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id,
+            self.x,
+            self.y,
+            self.width
+        )
 
-        def update(self, *args, **kwargs):
-            """
-            updates the instance attributes
+    def update(self, *args, **kwargs):
+        """
+        updates the instance attributes
 
-            Args:
-                args (tuple): non-word arguments to update the attributes
-                kwargs (dictionary): keyword arguments to update the attributes
+        Args:
+            args (tuple): non-word arguments to update the attributes
+            kwargs (dictionary): keyword arguments to update the attributes
 
-            Returns:
-                None
-            """
+        Returns:
+            None
+        """
 
-            if len(args):
-                if len(args) >= 4:
+        if len(args):
+            if len(args) >= 4:
+                self.id = args[0]
+                self.width = args[1]
+                self.height = args[1]
+                self.x = args[2]
+                self.y = args[3]
+            else:
+                if len(args) >= 1:
                     self.id = args[0]
+                if len(args) >= 2:
                     self.width = args[1]
                     self.height = args[1]
+                if len(args) >= 3:
                     self.x = args[2]
+                if len(args) >= 4:
                     self.y = args[3]
-                else:
-                    if len(args) >= 1:
-                        self.id = args[0]
-                    if len(args) >= 2:
-                        self.width = args[1]
-                        self.height = args[1]
-                    if len(args) >= 3:
-                        self.x = args[2]
-                    if len(args) >= 4:
-                        self.y = args[3]
 
-            else:
-                if "id" in kwargs:
-                    self.id = kwargs.get("id")
-                if "size" in kwargs:
-                    self.width = kwargs.get("size")
-                    self.height = kwargs.get("size")
-                if "x" in kwargs:
-                    self.x = kwargs.get("x")
-                if "y" in kwargs:
-                    self.y = kwargs.get("y")
+        else:
+            if "id" in kwargs:
+                self.id = kwargs.get("id")
+            if "size" in kwargs:
+                self.width = kwargs.get("size")
+                self.height = kwargs.get("size")
+            if "x" in kwargs:
+                self.x = kwargs.get("x")
+            if "y" in kwargs:
+                self.y = kwargs.get("y")
