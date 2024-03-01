39c996c84bc717c087298f1f2d7f0616f1bf234e
= <=>
2024-03-01 13:13:58 -0800

    Square Update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,14 +13,6 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
-    def __str__(self):
-        return "[Square] ({}) {}/{} - {}".format(
-            self.id,
-            self.x,
-            self.y,
-            self.width
-        )
-
     @property
     def size(self):
         """
@@ -44,3 +36,52 @@ class Square(Rectangle):
         """
         self.width = size
         self.height = size
+
+        def __str__(self):
+            return "[Square] ({}) {}/{} - {}".format(
+                self.id,
+                self.x,
+                self.y,
+                self.width
+            )
+
+        def update(self, *args, **kwargs):
+            """
+            updates the instance attributes
+
+            Args:
+                args (tuple): non-word arguments to update the attributes
+                kwargs (dictionary): keyword arguments to update the attributes
+
+            Returns:
+                None
+            """
+
+            if len(args):
+                if len(args) >= 4:
+                    self.id = args[0]
+                    self.width = args[1]
+                    self.height = args[1]
+                    self.x = args[2]
+                    self.y = args[3]
+                else:
+                    if len(args) >= 1:
+                        self.id = args[0]
+                    if len(args) >= 2:
+                        self.width = args[1]
+                        self.height = args[1]
+                    if len(args) >= 3:
+                        self.x = args[2]
+                    if len(args) >= 4:
+                        self.y = args[3]
+
+            else:
+                if "id" in kwargs:
+                    self.id = kwargs.get("id")
+                if "size" in kwargs:
+                    self.width = kwargs.get("size")
+                    self.height = kwargs.get("size")
+                if "x" in kwargs:
+                    self.x = kwargs.get("x")
+                if "y" in kwargs:
+                    self.y = kwargs.get("y")
