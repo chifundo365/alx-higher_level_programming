f274e4d9eecd1c3e181f46066c2b51dfc53dbe5b
= <=>
2024-03-22 17:18:31 -0700

    List by best

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- List all records of the table 'second_table' of database hbtn_0c_0 in MySql Server
+-- Records are ordered by the score column (descending order)
+SELECT * FROM second_order ORDER BY score DESC;
