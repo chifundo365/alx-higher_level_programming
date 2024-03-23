cbaad2f3fef9a15dc55dfc62d8877b4825eded0b
= <=>
2024-03-22 17:35:09 -0700

    Select the best

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- displays the score and name in descending order based on score
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
