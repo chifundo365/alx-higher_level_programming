7e622201d194360a86ecad5ab9c89d8809b2842f
= <=>
2024-04-04 14:33:50 -0700

    Reda user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
--- Creates the databse hbtn_0d_2 and the user 'user_0d_2'
+-- creates a database hbtn_0d_2 and user user_0d_2 with select prev. on the database
 
 CREATE USER
 IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
