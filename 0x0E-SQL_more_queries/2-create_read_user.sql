db898a9265bb3305be5c6bd5002394117e93e33f
= <=>
2024-04-04 14:21:55 -0700

    Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
--- Creates the databse hbtn_0d_2 and the user 'user_0d_2
+-- Creates the databse hbtn_0d_2 and the user 'user_0d_2'
 
 CREATE USER
 IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
