9909458a3989b4e0e52a0fd50074b0ac233e8724
= <=>
2024-04-07 08:32:25 -0700

    NO genre

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- Lists all shows contained in hbtn_0d_tvshows without a genre linked
+SELECT tv_s.title, tv_s_g.genre_id
+FROM tv_shows tv_s
+LEFT JOIN tv_show_genres tv_s_g
+ON tv_s.id = tv_s_g.show_id
+WHERE tv_s_g.show_id IS NULL;
