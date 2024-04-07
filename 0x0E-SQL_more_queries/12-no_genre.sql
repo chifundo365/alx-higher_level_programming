f822b44244973fd259c459b127222581d223af08
= <=>
2024-04-07 08:36:21 -0700

    No genre

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -3,4 +3,5 @@ SELECT tv_s.title, tv_s_g.genre_id
 FROM tv_shows tv_s
 LEFT JOIN tv_show_genres tv_s_g
 ON tv_s.id = tv_s_g.show_id
-WHERE tv_s_g.show_id IS NULL;
+WHERE tv_s_g.show_id IS NULL
+ORDER BY tv_s.title, tv_s_g.genre_id;
