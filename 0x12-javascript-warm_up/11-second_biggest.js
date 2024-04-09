bcfd51180a9970952a255eaf2e1e5b5d39976de5
= <=>
2024-04-09 11:46:33 -0700

    Second biggest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const numbers = process.argv;
+
+if (numbers.length === 2 || numbers.length === 3) {
+  console.log(0);
+} else {
+  let biggest = numbers[2];
+  for (let i = 2; i < numbers.length; i++) {
+    const number = +numbers[i];
+    if (number > biggest) {
+      biggest = number;
+    }
+  }
+  let secondBiggest = +numbers[2];
+  for (let x = 2; x < numbers.length; x++) {
+    const number = +numbers[x];
+    if (number !== biggest && ((biggest - number) < (biggest - secondBiggest))) {
+      secondBiggest = number;
+    }
+  }
+  console.log(secondBiggest);
+}
