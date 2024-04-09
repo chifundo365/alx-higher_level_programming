aa6dc94a8fe6e3921aca70cc97f080b91cb13cf0
= <=>
2024-04-09 04:22:57 -0700

    Value of my argument;

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
 const argv = process.argv;
-if (argv.length === 2) console.log('no argument');
-if (argv.length >= 3) console.log(argv[2]);
+if (argv[2] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(argv[2])
+}
