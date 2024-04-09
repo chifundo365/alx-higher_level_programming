7d66435c28616e8280fdb64ebeff17d3bfa9a00c
= <=>
2024-04-09 04:54:06 -0700

    An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 let number = process.argv;
 number = +(number[2]);
 if (isNaN(number)) {
-  console.log('Not number');
+  console.log('Not a number');
 } else {
   console.log('My number: ' + number);
 }
