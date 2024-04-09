27e4979e63fed414745df23f4aec551ffbc6cf33
= <=>
2024-04-09 07:01:47 -0700

    Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-function factorial(number) {
+function factorial (number) {
   if (isNaN(number) || number === 1) {
     return (1);
   }
   return number * factorial(number - 1);
 }
 
-const n = process.argv[2];
+const n = +process.argv[2];
 console.log(factorial(n));
