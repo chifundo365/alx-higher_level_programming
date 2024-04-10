67b62bacec1cdacf108e0d55d6362b3cd807f75d
= <=>
2024-04-10 05:52:13 -0700

    rectangle #3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 1 && h > 1) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      let output = '';
+      for (let y = 0; y < this.width; y++) {
+	output += 'X';
+      }
+      console.log(output);
+    }
+  }
+}
+
+module.exports = Rectangle;
