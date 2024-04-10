7138c93f543d8093b30820898fa8fd18efba4e16
= <=>
2024-04-10 06:56:07 -0700

    Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,24 @@
+#!/usr/iiiibin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      super.print();
+    } else {
+      for (let i = 0; i < this.width; i++) {
+        let output = '';
+        for (let y = 0; y < this.height; y++) {
+          output += 'C';
+        }
+        console.log(output);
+      }
+    }
+  }
+}
+
+module.exports = Square;
