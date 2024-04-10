067a97b7e8a7c37dd2f5495159afbf2a394fee62
= <=>
2024-04-10 07:01:43 -0700

    Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const Sq = require('./5-square');
 
-class Square extends Rectangle {
+class Square extends Sq {
   constructor (size) {
     super(size, size);
   }
