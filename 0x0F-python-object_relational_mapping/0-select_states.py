11da3f866e8f9e4a3ba8551149510e4526b1d7bc
= <=>
2024-04-12 15:21:10 -0700

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,23 +3,26 @@
 Lists all states from the databse hbtn_0e_0_usa
 '''
 import MySQLdb
-import logging
+
+username = 'root'
+hostname = 'localhost'
+dbname = 'hbtn_0e_0_usa'
 
 
 def list_states():
+    ''' select all columns from states table in MySQL database server '''
     try:
         db_conn = MySQLdb.connect(
-                host='localhost',
-                user='root',
-                passwd='',
-                db='hbtn_0e_0_usa')
+                host=hostname,
+                user=username,
+                db=dbname)
 
         with db_conn.cursor() as cursor:
             cursor.execute('SELECT * FROM states ORDER BY id')
             for row in cursor.fetchall():
                 print(row)
     except MySQLdb.Error as e:
-        logging.error('error fetching data from database: {}'.format(e))
+        print(e)
 
     finally:
         if cursor:
