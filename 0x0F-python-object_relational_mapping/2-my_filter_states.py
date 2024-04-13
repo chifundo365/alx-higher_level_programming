3179c05f3b3008fb7eebf01ac60c65f27ed4ed44
= <=>
2024-04-13 13:59:29 -0700

    filter sates by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''
+Lists all states from the database hbtn_0e_0_usa where name matches the input
+'''
+import MySQLdb
+from sys import argv
+
+
+def select_states(username, password, dbname, col_name):
+    try:
+        db_conn = MySQLdb.connect(
+                host='localhost',
+                user=username,
+                passwd=password,
+                db=dbname)
+        cursor = db_conn.cursor()
+        sql = 'SELECT * FROM states WHERE name = %s ORDER BY id'
+        cursor.execute(sql, (col_name,))
+
+        for row in cursor.fetchall():
+            print(row)
+    except Exception as e:
+        print('mysql error: {}'.format(e))
+    finally:
+        if cursor:
+            cursor.close()
+        if db_conn:
+            db_conn.close()
+
+
+if __name__ == '__main__':
+    if len(argv) < 4:
+        print('usage: {} [username], [password], [dbname], [state name] \
+                '.format(argv[0]))
+    else:
+        select_states(argv[1], argv[2], argv[3], argv[4])
