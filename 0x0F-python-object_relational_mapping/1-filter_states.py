dcd88e2d98633540716af0128d2e59e1c9070102
= <=>
2024-04-13 13:38:12 -0700

    Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''
+Lists all states from the databse hbtn_0e_0_usa with name starting-
+with capital N
+'''
+import MySQLdb
+from sys import argv
+
+
+def select_states(username, password, dbname):
+    try:
+        db_conn = MySQLdb.connect(
+                host='localhost',
+                user=username,
+                passwd=password,
+                db=dbname)
+        cursor = db_conn.cursor()
+        cursor.execute('SELECT * FROM states WHERE name \
+                COLLATE utf8mb4_bin LIKE "N%" ORDER BY id')
+
+        for row in cursor.fetchall():
+            print(row)
+    except Exception as e:
+        print('mysql error: {}'.format(e))
+    finally:
+        if cursor:
+            cursor.close()
+        if db_conn:
+            db_conn.close()
+
+
+if __name__ == '__main__':
+    if len(argv) < 4:
+        print('usage: {} [username], [password], [dbname]'.format(argv[0]))
+    else:
+        select_states(argv[1], argv[2], argv[3])
