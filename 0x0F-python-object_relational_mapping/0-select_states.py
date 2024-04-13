c6cd654dae7ae74371522b4685a459319689cb35
= <=>
2024-04-13 13:06:05 -0700

    Get all sates

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,27 +3,23 @@
 Lists all states from the databse hbtn_0e_0_usa
 '''
 import MySQLdb
-
-username = 'root'
-hostname = 'localhost'
-dbname = 'hbtn_0e_0_usa'
+from sys import argv
 
 
-def list_states():
-    ''' select all columns from states table in MySQL database server '''
+def select_states(username, password, dbname):
     try:
         db_conn = MySQLdb.connect(
-                host=hostname,
+                host='localhost',
                 user=username,
+                passwd=password,
                 db=dbname)
+        cursor = db_conn.cursor()
+        cursor.execute('SELECT * FROM states ORDER BY id')
 
-        with db_conn.cursor() as cursor:
-            cursor.execute('SELECT * FROM states ORDER BY id')
-            for row in cursor.fetchall():
-                print(row)
-    except MySQLdb.Error as e:
-        print(e)
-
+        for row in cursor.fetchall():
+            print(row)
+    except Exception as e:
+        print('mysql error: {}'.format(e))
     finally:
         if cursor:
             cursor.close()
@@ -32,4 +28,7 @@ def list_states():
 
 
 if __name__ == '__main__':
-    list_states()
+    if len(argv) < 4:
+        print('usage: {} [username], [password], [dbname]'.format(argv[0]))
+    else:
+        select_states(argv[1], argv[2], argv[3])
