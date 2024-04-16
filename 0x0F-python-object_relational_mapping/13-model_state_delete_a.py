e2ce14e1a4dd9e8c15ca7b15e3912e25a2b02937
= <=>
2024-04-16 05:46:01 -0700

    Delete states

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''
+This script deletes the  state objects which contains 'a' in the names\
+        from the database 'hbtn_0e_6_usa'
+'''
+
+
+if __name__ == '__main__':
+    from sys import argv
+    from sqlalchemy import create_engine, delete
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                argv[1],
+                argv[2],
+                argv[3]
+                )
+            )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    rows = session.query(State).where(State.name.like('%a%')).all()
+    for row in rows:
+        session.delete(row)
+        session.commit()
