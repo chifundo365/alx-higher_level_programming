302072313aaa8e5f4dfbc7803a78e0540906fb17
= <=>
2024-04-16 05:08:09 -0700

    Add a new state

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -20,7 +20,6 @@ if __name__ == '__main__':
             )
     Session = sessionmaker(bind=engine)
     session = Session()
- 
     state = State()
     state.name = 'Louisiana'
     session.add(state)
