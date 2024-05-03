19c145d5db32ec551e93160a88591b13e598c1f4
= <=>
2024-05-02 19:20:27 -0700

    cURL only methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes an URL and display all HTTP methonds the server will accept
-curl -is -X OPTIONS "$1" | grep Allow: | sed 's/Allow://'
+curl -is -X OPTIONS "$1" | grep Allow: | sed 's/Allow: //'
