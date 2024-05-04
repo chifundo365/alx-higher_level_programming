300faa3467194723b6abc87c7077f96d8e06a901
= <=>
2024-05-04 03:04:12 -0700

    Find a peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Contains a function used to find a peak """
+
+
+def find_peak(list_of_integers):
+    """BRUTE force implementation for question """
+    max_i = None
+    for ele in list_of_integers:
+        if max_i is None or max_i < ele:
+            max_i = ele
+    return max_i
