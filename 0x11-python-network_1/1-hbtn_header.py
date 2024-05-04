cd88cb0d0d9f49707c65d06241943aef687597e8
= <=>
2024-05-04 12:39:39 -0700

    Response header vaalue #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
-""" Sends a request to an URL and displays the value of a header variable """
+""" 
+Sends a request to an URL and displays the value of a header variable
+
+This script takes a URL as a command-line argument and sends a request to that URL. It then searches
+for a specific header variable, 'X-Request-Id', in the response headers and prints its value.
+
+Usage:
+    python3 script_name.py <url>
+
+Example:
+    python3 script_name.py http://example.com
+
+Dependencies:
+    This script requires Python 3, sys module and the urllib module.
+"""
+
 from urllib import request
 from sys import argv
 
