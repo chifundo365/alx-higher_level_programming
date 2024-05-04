b9146cfc54842ca03a3d711f02e0be4f49c49710
= <=>
2024-05-04 04:26:58 -0700

    What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,6 +5,7 @@ import urllib.request
 
 with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as res:
     _bytes = res.read()
+    print("Body response:")
     print('\t- type: {}'.format(type(_bytes)))
     print('\t- content: {}'.format(_bytes))
     print('\t- utf8 content: {}'.format(_bytes.decode('utf-8')))
