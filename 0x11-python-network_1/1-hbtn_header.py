d6a80587d624effe8ab8682b5577577f02499fd4
= <=>
2024-05-04 12:14:43 -0700

    Response header vaalue #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""
-Sends a request to an URL and displays the value of a header variable
-"""
+"""Sends a request to an URL and displays the value of a header variable"""
 from urllib import request
 from sys import argv
+
 
 with request.urlopen(argv[1]) as res:
     for k, v in res.headers.items():
