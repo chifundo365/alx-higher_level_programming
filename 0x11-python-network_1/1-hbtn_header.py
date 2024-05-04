a8cad5db883c33e4b8aca3383312b6ddcafec8d6
= <=>
2024-05-04 12:50:29 -0700

    Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,10 +3,11 @@
 Python script that takes in a URL, sends a request and displays
 the value of the X-Request-Id variable found in the header
 """
+import urllib.request
+import sys
 
-from urllib import request
-from sys import argv
 
-
-with request.urlopen(argv[1]) as res:
-    print(res.headers.get("X-Request-Id"))
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        html = response.info()
+        print(html.get('X-Request-Id'))
